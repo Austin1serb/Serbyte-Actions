adc0acefe26debddd1c3d7d68a2fa64228ea4354
Austin1serb <serbaustin@gmail.com>
2025-04-23 12:41:25 -0700

    update to html_body email

--- dist/index.js
+++ dist/index.js
@@ -35691,38 +35691,26 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 async function run() {
     try {
         const openai = new openai_1.OpenAI({ apiKey: OPENAI_API_KEY });
-        core.notice(`OPENAI_API_KEY: ${OPENAI_API_KEY}`);
         if (!OPENAI_API_KEY)
             throw new Error("OPENAI_API_KEY is not set");
         const model = core.getInput("openai-model") || "gpt-4o-mini";
-        core.notice(`model: ${model}`);
         const commits = github.context.payload.commits ?? [];
         if (!commits.length)
             throw new Error("No commits in payload");
-        core.notice(`commits: ${commits.length}`);
         // Prepare commit messages
         const commitMessages = commits.map((c) => `- ${c.message.trim()}`).join("\n");
-        core.notice(`commitMessages: ${commitMessages}`);
-        core.notice(`systemPrompt: ${assets_1.systemPrompt}`);
-        core.notice(`commitMessages: ${commitMessages}`);
-        core.notice(`emailTemplate: ${assets_1.emailTemplate}`);
-        core.notice(`Test Openai API call: ${await openai.models.list()}`);
         // Call OpenAI API
         const completion = await openai.chat.completions.create({
-            model: "gpt-4o-mini",
+            model: model,
             messages: [
                 { role: "system", content: assets_1.systemPrompt },
                 { role: "user", content: commitMessages }
             ]
         });
-        core.notice(`completion: ${completion}`);
-        core.notice(`completion.choices: ${completion.choices}`);
         const summary = completion?.choices?.[0]?.message?.content?.trim();
-        core.notice(`summary: ${summary}`);
         if (!summary)
             throw new Error("Empty OpenAI response");
         const repoName = github.context.repo.repo;
-        core.notice(`repoName: ${repoName}`);
         // Fill {{BODY}} and {{REPO_NAME}} inside full email template
         const finalEmail = assets_1.emailTemplate.replace("{{BODY}}", summary).replace(/{{REPO_NAME}}/g, repoName);
         core.notice(`finalEmail: ${finalEmail}`);
